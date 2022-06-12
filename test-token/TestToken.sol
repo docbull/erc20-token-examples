0e5925b367b29a51e371606c8b99f71fd249a919
docbull <fudghks21@gmail.com>
2022-06-13 02:35:07 +0900

    ERC-20 token based customized test token

--- test-token/TestToken.sol
+++ test-token/TestToken.sol
@@ -1,58 +1,23 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.14;
 
-contract TestToken {
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "./node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    string public constant name = "Test Token Coin"; // the name that will be shown on wallet
-    string public constant symbol = "TTC";           // it indicates the coin's symbol (e.g., ETH from ethereum)
-    uint8 public constant decimals = 18;             // this number means the decimal of the amount of the coin
+contract TestToken is ERC20 {
+    string private constant _name = "Test Token Coin"; // the name that will be shown on wallet
+    string private constant _symbol = "TTC";           // it indicates the coin's symbol (e.g., ETH from Ethereum)
+    uint8 private constant _decimals = 18;             // this number means the decimal of the amount of the coin
 
-    mapping(address => uint256) balances;
-    mapping(address => mapping (address => uint256)) allowed;
+    uint256 private _initial_supply;
 
-    uint256 totalSupply_;
+    constructor(uint total) ERC20(_name, _symbol) public {
+        _initial_supply = total * (10 ** _decimals);
 
-    constructor(uint256 total) {
-      totalSupply_ = total * (10**18);
-      balances[msg.sender] = totalSupply_;
+        _mint(msg.sender, _initial_supply);
     }
 
-    function totalSupply() public view returns (uint256) {
-      return totalSupply_;
-    }
-
-    function balanceOf(address tokenOwner) public view returns (uint) {
-        return balances[tokenOwner];
-    }
-
-    function transfer(address receiver, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[msg.sender]);
-        balances[msg.sender] -= numTokens;
-        balances[receiver] += numTokens;
-        emit Transfer(msg.sender, receiver, numTokens);
-        return true;
-    }
-
-    function approve(address delegate, uint numTokens) public returns (bool) {
-        allowed[msg.sender][delegate] = numTokens;
-        emit Approval(msg.sender, delegate, numTokens);
-        return true;
-    }
-
-    function allowance(address owner, address delegate) public view returns (uint) {
-        return allowed[owner][delegate];
-    }
-
-    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[owner]);
-        require(numTokens <= allowed[owner][msg.sender]);
-
-        balances[owner] -= numTokens;
-        allowed[owner][msg.sender] -= numTokens;
-        balances[buyer] += numTokens;
-        emit Transfer(owner, buyer, numTokens);
-        return true;
-    }
+    // function balanceOf(address account) public view virtual override returns (uint256) {
+    //     return _balances[account];
+    // }
 }
