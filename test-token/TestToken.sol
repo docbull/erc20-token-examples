5a3f01792b40dd4ce42f69508becac4716eee62b
docbull <fudghks21@gmail.com>
2022-06-14 22:11:59 +0900

    testing functions are added

--- test-token/TestToken.sol
+++ test-token/TestToken.sol
@@ -7,10 +7,13 @@ import "./node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract TestToken is ERC20 {
     // Setttle is a settlement of that users' rewards
     event Settle(address indexed from, address indexed to, uint tokens);
+    event Record(address indexed user, uint chunk);
 
     string private constant _name = "Test Token Coin"; // the name that will be shown on wallet
     string private constant _symbol = "TTC";           // it indicates the coin's symbol (e.g., ETH from Ethereum)
     uint8 private constant _decimals = 18;             // this number means the decimal of the amount of the coin
+
+    mapping(address => uint256) records;
 
     uint256 private _initial_supply;
 
@@ -23,6 +26,20 @@ contract TestToken is ERC20 {
     function settle(address receiver, uint numTokens) public returns (bool) {
         emit Settle(msg.sender, receiver, numTokens);
         return true;
+    }
+
+    function payment(uint numTokens) public returns (bool) {
+        return true;
+    }
+
+    function record(uint chunk) public returns (bool) {
+        require(chunk <= records[msg.sender]);
+        records[msg.sender] += chunk;
+        return true;
+    }
+
+    function inquery(address tokenOwner) public view returns (uint) {
+        return records[tokenOwner];
     }
 
     // function balanceOf(address account) public view virtual override returns (uint256) {
