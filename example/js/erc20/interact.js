a3fb0cdb22039e197bca670f098aea854e4c9a24
docbull <fudghks21@gmail.com>
2022-06-18 20:07:58 +0900

    balances of account function is added

--- example/js/erc20/interact.js
+++ example/js/erc20/interact.js
@@ -1,15 +1,11 @@
 import contractABI from './TestToken-abi.json'assert{type: "json"};
 const contractAddress = "0xAB8c361D9f7Fd5f0C32261F625a7C181adD6C5e5";
-const alchemyKey = "";
+const alchemyKey = "https://eth-rinkeby.alchemyapi.io/v2/-Ji3kkJnrDIJC6fOd68MhCOTXijRu1qy";
 const web3 = AlchemyWeb3.createAlchemyWeb3(alchemyKey);
 
 export async function tokenTransfer () {
     if (window.ethereum) {
         window.contract = await new web3.eth.Contract(contractABI, contractAddress);
-        let metamaskAccount = window.ethereum.selectedAddress;
-        const balances = await window.contract.methods.balanceOf(metamaskAccount).call();
-        console.log("Balances:", balances/(10**18));
-
         const value = web3.utils.toBN(1 * (10**18));
         const data = await  window.contract.methods.transfer(contractAddress, value);
         const transactionParameters = {
@@ -28,4 +24,28 @@ export async function tokenTransfer () {
     } else {
         console.log("NONE OF ETHERUEM");
     }
+}
+
+export async function getBalances() {
+    let messages;
+    if (window.ethereum) {
+        window.contract = await new web3.eth.Contract(contractABI, contractAddress);
+        const balances = await window.contract.methods.balanceOf(window.ethereum.selectedAddress).call();
+        console.log("Balances:", balances/(10**18));
+        messages = `
+            <div>
+                Account: ${window.ethereum.selectedAddress}
+            <div>
+            <div>
+                Balances: ${balances/(10**18)}
+            <div>
+        `
+    } else {
+        messages = `
+            <div>
+                Your need to login
+            <div>
+        `
+    }
+    document.getElementById("user-info").innerHTML += messages;
 }
