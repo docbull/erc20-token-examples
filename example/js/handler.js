db3abaa524dba331b2f695cf5cebfff33d75a571
docbull <fudghks21@gmail.com>
2022-06-18 20:08:19 +0900

    shows account info when the page is loaded

--- example/js/handler.js
+++ example/js/handler.js
@@ -1,6 +1,7 @@
-import { tokenTransfer } from "./erc20/interact.js";
+import { tokenTransfer, getBalances } from "./erc20/interact.js";
 
 window.onload = function () {
+    getBalances();
     $("#transfer").click(function () {
         console.log("💰 Tanferring 1 token ...");
         tokenTransfer();
