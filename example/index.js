59322532f835c725764cc04f5502b07db9e12dfb
docbull <fudghks21@gmail.com>
2022-06-29 22:35:50 +0900

    express based ERC-20 token example

--- example/index.js
+++ example/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+const options = {
+    key: fs.readFileSync('./ssl/rootca.key'),
+    cert: fs.readFileSync('./ssl/rootca.crt')
+};
 
 const app = express();
 
@@ -13,6 +21,11 @@ const router = require('./router/route')(app);
 app.use(express.static(path.join(__dirname, '/js')));
 app.use(express.static(path.join(__dirname, '/img')));
 
-const server = app.listen(app.get('port'), () => {
+// const server = app.listen(app.get('port'), () => {
+//     console.log(app.get('port'), 'is open 😀');
+// });
+
+http.createServer(app).listen(app.get('port'), () => {
     console.log(app.get('port'), 'is open 😀');
-});
+})
+// https.createServer(options, app).listen(app.get('port'));
